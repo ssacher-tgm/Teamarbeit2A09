5521534243bdea1479059fb617174a678461db60
Sebastian <ssacher@student.tgm.ac.at>
2015-12-17 17:23:40 +0100

    added View and Controller

--- Teamarbeit2/src/controller/Main_Controller.java
+++ Teamarbeit2/src/controller/Main_Controller.java
@@ -1,10 +1,61 @@
 package controller;
 
-public class Main_Controller {
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+import view.GUI_View;
+
+public class Main_Controller implements ActionListener {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		@SuppressWarnings("unused")
+		GUI_View v = new GUI_View();
 	}
 
+	int a;
+	int b;
+	boolean c = true;
+	public int counterV = 0;
+	public static JButton[][] buttons = GUI_View.buttons;
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		for(int i=0;i<buttons.length;i++){
+			for(int j=0;j<buttons.length;j++){
+				if(e.getSource().equals(buttons[i][j])){
+					if(buttons[i][j].getBackground()==Color.BLACK){
+						buttons[i][j].setBackground(Color.GREEN);
+					} else buttons[i][j].setBackground(Color.BLACK);
+					
+					if(i<5/**ränder und ecken**/){
+						if(buttons[i][j].getBackground()==Color.BLACK){
+							buttons[i][j].setBackground(Color.GREEN);
+						} else buttons[i][j].setBackground(Color.BLACK);
+					}
+					
+					if(buttons[i-1][j].getBackground()==Color.BLACK){
+						buttons[i-1][j].setBackground(Color.GREEN);
+					} else buttons[i-1][j].setBackground(Color.BLACK);
+					
+					if(buttons[i][j+1].getBackground()==Color.BLACK){
+						buttons[i][j+1].setBackground(Color.GREEN);
+					} else buttons[i][j+1].setBackground(Color.BLACK);
+					
+					if(buttons[i][j-1].getBackground()==Color.BLACK){
+						buttons[i][j-1].setBackground(Color.GREEN);
+					} else buttons[i][j-1].setBackground(Color.BLACK);
+				}
+			}
+		}
+		if(e.getSource().equals(GUI_View.newGame)){
+			for(int i=0;i<buttons.length;i++){
+				for(int j=0;j<buttons.length;j++){
+					buttons[i][j].setBackground(Color.BLACK);
+				}
+			}
+		}
+	}
 }
--- Teamarbeit2/src/view/GUI_View.java
+++ Teamarbeit2/src/view/GUI_View.java
@@ -1,8 +1,77 @@
 package view;
 
-public class GUI_View {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
 
-	public static void main(String[] args) {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import controller.Main_Controller;
+
+@SuppressWarnings("serial")
+public class GUI_View extends JFrame{
+
+	private static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	private static int DEF_X_POS = (int) screenSize.getWidth() / 4 + 250;
+	private static int DEF_Y_POS = (int) screenSize.getHeight() / 4;
+	private static int X_DIM = 500;
+	private static int Y_DIM = 500;
+	
+	private JPanel mainPanel;
+	private JPanel middlePanel;
+	private JPanel topPanel;
+	
+	public static JButton[][] buttons = new JButton[5][5];
+	
+	private Main_Controller mC = new Main_Controller();
+	
+	public static JButton newGame = new JButton("NEUES SPIEL");
+
+	public static JLabel counter = new JLabel();
+
+	public GUI_View() {
+		super("Lights Out (5x5)");
+		mainPanel = new JPanel(new BorderLayout());
+		middlePanel = new JPanel(new GridLayout(5, 5));
+		topPanel = new JPanel(new GridLayout(1, 2));
+		this.getContentPane().add(mainPanel);
+
+		mainPanel.add(middlePanel, BorderLayout.CENTER);
+		mainPanel.add(topPanel, BorderLayout.NORTH);
+
+		newGame.setBackground(Color.BLACK);
+		newGame.setForeground(Color.WHITE);
+		newGame.addActionListener(mC);
+		topPanel.add(newGame, BorderLayout.NORTH);
+		
+		counter.setBackground(Color.BLACK);
+		counter.setForeground(Color.WHITE);
+		counter.setOpaque(true);
+		counter.setText("Spielzug 0");
+		counter.setHorizontalAlignment(JLabel.CENTER);
+		topPanel.add(counter, BorderLayout.NORTH);
+		
+		//System.out.println(buttons.length);
+		for(int i=0;i<buttons.length;i++){
+			for(int j=0;j<buttons.length;j++){
+				buttons[i][j]=new JButton();
+				middlePanel.add(buttons[i][j]);
+				buttons[i][j].addActionListener(mC);
+				buttons[i][j].setBackground(Color.BLACK);
+			}
+		}
+		
+		this.setBackground(Color.BLACK);
+		this.setResizable(false);
+		this.pack();
+		this.setLocation(DEF_X_POS, DEF_Y_POS);
+		this.setSize(new Dimension(X_DIM, Y_DIM));
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
 	}
-
 }
